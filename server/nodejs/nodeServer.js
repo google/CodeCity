a9c95e81f9e16453c3973556da47ae0ba28588c7
Neil Fraser <fraser@google.com>
2017-03-10 12:25:34 +0000

    Add Node.js server for parsing code to AST using Acorn.

---
+++ server/nodejs/nodeServer.js
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Code City Node.js Server
+ *
+ * Copyright 2017 Google Inc.
+ * https://github.com/NeilFraser/CodeCity
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Node.js server that provides JavaScript services to Code City.
+ * @author fraser@google.com (Neil Fraser)
+ */
+
+// Start with: node nodeServer.js
+'use strict';
+
+var net = require('net');
+var acorn = require('./acorn.js');
+
+// Create a new TCP server for parsing code into an AST using Acorn.
+var parsePort = 7780;
+var server = net.createServer(function (socket) {
+  console.log('Parse connection from ' + socket.remoteAddress);
+  socket.incomingData_ = '';
+
+  // Handle incoming messages from clients.
+  socket.on('data', function (data) {
+    socket.incomingData_ += String(data).replace('\r', '');
+    // Incoming data is terminated by a '.' on its own line.
+    var end = socket.incomingData_.lastIndexOf('\n.\n');
+    if (end != -1) {
+      var code = socket.incomingData_.substring(0, end);
+      try {
+        var ast = acorn.parse(code);
+        socket.write(JSON.stringify(ast));
+        console.log('Parse: ' + code.length + ' bytes.');
+      } catch (e) {
+        // Syntax error in provided code.
+        var error = {'type': e.name, 'message': e.message, 'error': e};
+        socket.write(JSON.stringify(error));
+        console.log('Parse: ' + e);
+      }
+      socket.end('\n');
+    }
+  });
+});
+server.listen(parsePort, 'localhost');
+console.log('Parse server listening on port ' + parsePort);
