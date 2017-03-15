0160019e725a37f988f33b1c5a8684e685614ce4
Christopher Allen <cpcallen+git@gmail.com>
2017-03-15 11:57:05 +0000

    Unmarshal []statement (as Statements)

--- server/interpreter/ast/ast.go
+++ server/interpreter/ast/ast.go
@@ -3,8 +3,9 @@ package ast
 //go:generate sh -c "./md2go es5.md > astnodes.go && go fmt"
 
 import (
-// "encoding/json"
-// "fmt"
+	"encoding/json"
+	"fmt"
+	"reflect"
 )
 
 type node interface {
@@ -13,21 +14,67 @@ type node interface {
 
 func (nodeStuff) _is_node() {}
 
-type Statement statement
-type Statements []statement
 type statement interface {
 	_is_statement()
 }
 
 func (statementStuff) _is_statement() {}
 
-type Expression expression
+type Statement struct{ statement }
+type Statements []statement
+
+// func (this *Statement) UnmarshalJSON(b []byte) error {
+// 	var tmp thingWithType
+// 	if err := json.Unmarshal(b, &tmp); err != nil {
+// 		return err
+// 	}
+// 	var s statement = reflect.New(reflect.TypeOf(statementTypes[tmp.Type]).
+// 		Elem()).Interface().(statement)
+// 	if err := json.Unmarshal(b, &s); err != nil {
+// 		return err
+// 	}
+// 	this.statement = s
+// 	return nil
+// }
+
+func (this *Statements) UnmarshalJSON(b []byte) error {
+	var tmp []struct {
+		Type string `json:"type"`
+	}
+	if err := json.Unmarshal(b, &tmp); err != nil {
+		return err
+	}
+	s := make([]statement, len(tmp))
+	for i, t := range tmp {
+		fmt.Printf("Type = %s\n", t.Type)
+		stype, ok := statementTypes[t.Type]
+		if !ok {
+			return fmt.Errorf("ast: json.Unmarshal: unknown type %s", t.Type)
+		}
+		s[i] = reflect.New(reflect.TypeOf(stype).Elem()).Interface().(statement)
+	}
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+	*this = s
+	return nil
+}
+
+type Expression struct{ expression }
 type Expressions []expression
 type expression interface {
 	_is_expression()
 }
 
 func (expressionStuff) _is_expression() {}
+
+func (this *Expression) UnmarshalJSON(b []byte) error {
+	return nil
+}
+
+func (this *Expressions) UnmarshalJSON(b []byte) error {
+	return nil
+}
 
 type DeclOrId declOrId
 type declOrId interface {
