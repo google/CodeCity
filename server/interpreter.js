NeilFraser/JS-Interpreter

Add support for function in the nativeToPseudo

src/interpreter.js
@@ -1892,6 +1892,23 @@ Interpreter.prototype.nativeToPseudo = function(nativeObj) {
       nativeObj instanceof RegExp) {
     return this.createPrimitive(nativeObj);
   }
+
+  if (nativeObj instanceof Function) {
+    var wrapper = (function(interpreter, nativeObj){
+      return function() {
+        return interpreter.createPrimitive(
+          nativeObj.apply(interpreter,
+            Array.prototype.slice.call(arguments)
+              .map(function(i) {
+                return interpreter.pseudoToNative(i);
+              })
+          )
+        );
+      };
+    })(this, nativeObj);
+    return this.createNativeFunction(wrapper);
+  }
+
   var pseudoObj;
   if (nativeObj instanceof Array) {  // Array.
     pseudoObj = this.createObject(this.ARRAY);

google/CodeCity

server/interpreter.js
@@ -1945,6 +1945,23 @@ Interpreter.prototype.nativeToPseudo = function(nativeObj) {
       nativeObj instanceof RegExp) {
     return this.createPrimitive(nativeObj);
   }
+
+  if (nativeObj instanceof Function) {
+    var wrapper = (function(interpreter, nativeObj){
+      return function() {
+        return interpreter.nativeToPseudo(
+          nativeObj.apply(interpreter,
+            Array.prototype.slice.call(arguments)
+              .map(function(i) {
+                return interpreter.pseudoToNative(i);
+              })
+          )
+        );
+      };
+    })(this, nativeObj);
+    return this.createNativeFunction(wrapper);
+  }
+
   var pseudoObj;
   if (nativeObj instanceof Array) {  // Array.
     pseudoObj = this.createObject(this.ARRAY);