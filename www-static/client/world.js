a6576ff0a559023b49c3c4f7418f7b4c004259e9
Neil Fraser <fraser@google.com>
2017-04-07 19:47:34 +0100

    Add blank frames for comics.

--- www-static/client/world.js
+++ www-static/client/world.js
@@ -38,10 +38,25 @@ CCC.World.maxHistorySize = 10000;
 CCC.World.history = [];
 
 /**
+ * Height of history panels.
+ * @constant
+ */
+CCC.World.panelHeight = 256;
+
+/**
+ * PID of rate-limiter for resize events.
+ */
+CCC.World.resizePid = 0;
+
+/**
  * Initialization code called on startup.
  */
 CCC.World.init = function() {
+  CCC.World.historyDiv = document.getElementById('historyDiv');
+  CCC.World.currentDiv = document.getElementById('currentDiv');
   CCC.World.parser = new DOMParser();
+
+  window.addEventListener('resize', CCC.World.resize, false);
 
   // Report back to the parent frame that we're fully loaded and ready to go.
   parent.postMessage('initWorld', location.origin);
@@ -69,6 +84,73 @@ CCC.World.receiveMessage = function(e) {
       console.log(dom);
     }
   }
+};
+
+/**
+ * Rerender entire history.
+ * Called when the window changes size.
+ */
+CCC.World.resize = function() {
+  clearTimeout(CCC.World.resizePid);
+  CCC.World.resizePid = setTimeout(CCC.World.renderHistory, 1000);
+};
+
+/**
+ * Rerender entire history.
+ * Called when the window changes size.
+ */
+CCC.World.renderHistory = function() {
+  var panelBloat = 2 * (5 + 2);  // Margin and border widths must match the CSS.
+  CCC.World.historyDiv.innerHTML = '';
+  for (var y = 0; y < 3; y++) {
+    var rowWidths = CCC.World.rowWidths();
+    var rowDiv = document.createElement('div');
+    rowDiv.className = 'historyRow';
+    for (var x = 0; x < rowWidths.length; x++) {
+      var panelDiv = document.createElement('div');
+      panelDiv.className = 'historyPanel';
+      panelDiv.style.height = (CCC.World.panelHeight) + 'px';
+      panelDiv.style.width = (rowWidths[x] - panelBloat) + 'px';
+      rowDiv.appendChild(panelDiv);
+    }
+    CCC.World.historyDiv.appendChild(rowDiv);
+  }
+};
+
+/**
+ * Given the current window width, assign the number and widths of panels on
+ * one history row.
+ * @return {!Array.<number>} Array of lengths.
+ */
+CCC.World.rowWidths = function() {
+  var windowWidth = CCC.World.historyDiv.offsetWidth;
+  var idealWidth = CCC.World.panelHeight * 5 / 4;  // Standard TV ratio.
+  var panelCount = Math.round(windowWidth / idealWidth);
+  var averageWidth = Math.floor(windowWidth / panelCount);
+  averageWidth = Math.max(averageWidth, CCC.World.panelHeight);
+  var smallWidth = Math.round(averageWidth * 0.9);
+  var largeWidth = averageWidth * 2 - smallWidth;
+  // Build an array of lengths.  Add in matching pairs.
+  var panels = [];
+  for (var i = 0; i < Math.floor(panelCount / 2); i++) {
+    if (Math.random() > 0.5) {
+      panels.push(averageWidth, averageWidth);
+    } else {
+      panels.push(smallWidth, largeWidth);
+    }
+  }
+  // Odd number of panels has one in the middle.
+  if (panels.length < panelCount) {
+    panels.push(averageWidth);
+  }
+  // Shuffle the array.
+  for (var i = panels.length; i; i--) {
+    var j = Math.floor(Math.random() * i);
+    var temp = panels[i - 1];
+    panels[i - 1] = panels[j];
+    panels[j] = temp;
+  }
+  return panels;
 };
 
 
