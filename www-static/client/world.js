a837ae7c5db413c303f432726a0c50e5372e2f81
Neil Fraser <fraser@google.com>
2017-04-11 21:34:00 +0100

    Minimally functional world view.

--- www-static/client/world.js
+++ www-static/client/world.js
@@ -49,6 +49,23 @@ CCC.World.panoramaMessages = [];
 CCC.World.panelHeight = 256;
 
 /**
+ * Width of planned history panels.
+ */
+CCC.World.panelWidths = [];
+
+/**
+ * Div containing a partial row of history panels (null if new row needed).
+ * @type Element
+ */
+CCC.World.historyRow = null;
+
+/**
+ * Data from the last room seen (background, users, contents).
+ * @type Object
+ */
+CCC.World.latestRoom = null;
+
+/**
  * PID of rate-limiter for resize events.
  */
 CCC.World.resizePid = 0;
@@ -58,6 +75,24 @@ CCC.World.resizePid = 0;
  * in a change of width.
  */
 CCC.World.lastWidth = NaN;
+
+/**
+ * Namespace for SVG elements.
+ * @constant
+ */
+CCC.World.NS = 'http://www.w3.org/2000/svg';
+
+/**
+ * Scratchpad for rendering potential history panels.  SVG or iframe.
+ * @type {Element}
+ */
+CCC.World.scratchHistory = null;
+
+/**
+ * Scratchpad for rendering potential panorama panels.  SVG or iframe.
+ * @type {Element}
+ */
+CCC.World.scratchPanorama = null;
 
 /**
  * Initialization code called on startup.
@@ -87,8 +122,14 @@ CCC.World.receiveMessage = function(e) {
   var mode = e.data['mode'];
   var text = e.data['text'];
   if (mode == 'clear') {
-    // TODO: Clear history.
+    CCC.World.historyMessages.length = 0;
+    CCC.World.panoramaMessages.length = 0;
+    CCC.World.renderHistory();
   } else if (mode == 'message') {
+    if (text.indexOf('I don\'t understand that.') != -1) {
+      // <say user="Max" room="The Hangout">Hello world.</say>
+      text = '<iframe src="http://purple.com"></iframe>';
+    }
     var dom = CCC.World.parser.parseFromString(text, 'text/xml');
     if (dom.getElementsByTagName('parsererror').length) {
       // Not valid XML, treat as string literal.
@@ -104,13 +145,28 @@ CCC.World.receiveMessage = function(e) {
  * @param {string|!Element} msg Message to render.
  */
 CCC.World.renderMessage = function(msg) {
+  if (!CCC.World.panelWidths.length) {
+    CCC.World.panelWidths = CCC.World.rowWidths();
+  }
+
+  if (msg.firstChild && msg.firstChild.tagName == 'iframe') {
+    // It's an iframe, create the DOM element that will be associated with this
+    // message for the rest of its life.
+    msg.iframe = document.createElement('iframe');
+    msg.iframe.sandbox = 'allow-forms allow-scripts';
+    msg.iframe.src = msg.firstChild.getAttribute('src');
+    msg.removeChild(msg.firstChild);  // Throw away redundant information.
+  }
+  var backupScratchHistory = CCC.World.scratchHistory;
   if (CCC.World.prerenderHistory(msg) && CCC.World.prerenderPanorama(msg)) {
     CCC.World.publishPanorama();
+    CCC.World.panoramaMessages.push(msg);
   } else {
-    CCC.World.rollbackHistory();
+    // Failure to render.  Revert to previous state.
+    CCC.World.scratchHistory = backupScratchHistory;
+    // Publish one panel to the history.
     CCC.World.publishHistory();
-    CCC.World.newHistory();
-    CCC.World.newPanorama();
+    // Try again.
     CCC.World.renderMessage(msg);
   }
 };
@@ -121,6 +177,21 @@ CCC.World.renderMessage = function(msg) {
  * @return {boolean} True if the message fit.  False if overflow.
  */
 CCC.World.prerenderHistory = function(msg) {
+  // For now every message needs its own frame.
+  if (CCC.World.panoramaMessages.length) {
+    return false;
+  }
+  if (msg.iframe) {
+    CCC.World.scratchHistory = msg.iframe;
+    return true;
+  }
+  var svg = CCC.World.createSvg();
+  var text = document.createElementNS(CCC.World.NS, 'text');
+  text.appendChild(document.createTextNode(msg));
+  text.setAttribute('x', 10);
+  text.setAttribute('y', 50);
+  svg.appendChild(text);
+  CCC.World.scratchHistory = svg;
   return true;
 };
 
@@ -130,6 +201,21 @@ CCC.World.prerenderHistory = function(msg) {
  * @return {boolean} True if the message fit.  False if overflow.
  */
 CCC.World.prerenderPanorama = function(msg) {
+  // For now every message needs its own frame.
+  if (CCC.World.panoramaMessages.length) {
+    return false;
+  }
+  if (msg.iframe) {
+    CCC.World.scratchPanorama = msg.iframe;
+    return true;
+  }
+  var svg = CCC.World.createSvg();
+  var text = document.createElementNS(CCC.World.NS, 'text');
+  text.appendChild(document.createTextNode(msg));
+  text.setAttribute('x', 20);
+  text.setAttribute('y', 70);
+  svg.appendChild(text);
+  CCC.World.scratchPanorama = svg;
   return true;
 };
 
@@ -137,30 +223,58 @@ CCC.World.prerenderPanorama = function(msg) {
  * Publish the previously experimentally rendered history frame to the user.
  */
 CCC.World.publishHistory = function() {
+  if (!CCC.World.historyRow) {
+    var rowDiv = document.createElement('div');
+    rowDiv.className = 'historyRow';
+    CCC.World.scrollDiv.insertBefore(rowDiv, CCC.World.panoramaDiv);
+    CCC.World.historyRow = rowDiv;
+  }
+  var width = CCC.World.panelWidths.shift();
+  var panelDiv = document.createElement('div');
+  panelDiv.className = 'historyPanel';
+  if (Math.random() < 1 / 16) {
+    // The occasional panel should lack a border for artistic reasons.
+    // TODO: change this to #fff when confident in history behaviour.
+    panelDiv.style.borderColor = '#ccc';
+  }
+  panelDiv.style.height = CCC.World.panelHeight + 'px';
+  panelDiv.style.width = width + 'px';
+  panelDiv.appendChild(CCC.World.scratchHistory);
+  CCC.World.scratchHistory.style.visibility = 'visible';
+  CCC.World.scratchHistory = null;
+  CCC.World.historyRow.appendChild(panelDiv);
+  CCC.World.scrollDiv.scrollTop = CCC.World.scrollDiv.scrollHeight;
+
+  if (!CCC.World.panelWidths.length) {
+    CCC.World.historyRow = null;  // Next row.
+  }
+  // Move all panorama messages into history.
+  Array.prototype.push.apply(CCC.World.historyMessages,
+                             CCC.World.panoramaMessages);
+  CCC.World.panoramaMessages.length = 0;
 };
 
 /**
  * Publish the previously experimentally rendered panorama frame to the user.
  */
 CCC.World.publishPanorama = function() {
+  while (CCC.World.panoramaDiv.firstChild) {
+    CCC.World.panoramaDiv.removeChild(CCC.World.panoramaDiv.firstChild);
+  }
+  CCC.World.panoramaDiv.appendChild(CCC.World.scratchPanorama);
+  CCC.World.scratchPanorama.style.visibility = 'visible';
+  CCC.World.scratchPanorama = null;
 };
 
 /**
- * Throw out the experimentally rendered history frame and restore from backup.
+ * Create a blank, hidden SVG.
+ * @return {!Element} SVG element.
  */
-CCC.World.rollbackHistory = function() {
-};
-
-/**
- * Create a blank history frame.
- */
-CCC.World.newHistory = function() {
-};
-
-/**
- * Create a blank panorama frame.
- */
-CCC.World.newPanorama = function() {
+CCC.World.createSvg = function() {
+  var svg = document.createElementNS(CCC.World.NS, 'svg');
+  svg.style.visibility = 'hidden';
+  document.body.appendChild(svg);
+  return svg;
 };
 
 /**
@@ -190,15 +304,6 @@ CCC.World.resizeNow = function() {
   }
   CCC.World.lastWidth = width;
   CCC.World.renderHistory();
-  CCC.World.renderPanorama();
-  CCC.World.scrollDiv.scrollTop = CCC.World.scrollDiv.scrollHeight;
-};
-
-/**
- * Rerender panorama panel.
- * Called when the window changes size.
- */
-CCC.World.renderPanorama = function() {
 };
 
 /**
@@ -206,26 +311,26 @@ CCC.World.renderPanorama = function() {
  * Called when the window changes size.
  */
 CCC.World.renderHistory = function() {
-  var panelBloat = 2 * (5 + 2);  // Margin and border widths must match the CSS.
   // Destroy all existing history.
   var historyRows = document.getElementsByClassName('historyRow');
   while (historyRows[0]) {
     historyRows[0].parentNode.removeChild(historyRows[0]);
   }
-  // Create new history.
-  for (var y = 0; y < 3; y++) {
-    var rowWidths = CCC.World.rowWidths();
-    var rowDiv = document.createElement('div');
-    rowDiv.className = 'historyRow';
-    for (var x = 0; x < rowWidths.length; x++) {
-      var panelDiv = document.createElement('div');
-      panelDiv.className = 'historyPanel';
-      panelDiv.style.height = (CCC.World.panelHeight) + 'px';
-      panelDiv.style.width = (rowWidths[x] - panelBloat) + 'px';
-      rowDiv.appendChild(panelDiv);
-    }
-    CCC.World.scrollDiv.insertBefore(rowDiv, CCC.World.panoramaDiv);
+  while (CCC.World.panoramaDiv.firstChild) {
+    CCC.World.panoramaDiv.removeChild(CCC.World.panoramaDiv.firstChild);
   }
+  CCC.World.panelWidths.length = 0;
+  CCC.World.historyRow = null;
+  CCC.World.scratchHistory = null;
+  CCC.World.scratchPanorama = null;
+  // Create new history.
+  var messages = CCC.World.historyMessages.concat(CCC.World.panoramaMessages);
+  CCC.World.historyMessages.length = 0;
+  CCC.World.panoramaMessages.length = 0;
+  for (var i = 0; i < messages.length; i++) {
+    CCC.World.renderMessage(messages[i]);
+  }
+  CCC.World.scrollDiv.scrollTop = CCC.World.scrollDiv.scrollHeight;
 };
 
 /**
@@ -242,6 +347,9 @@ CCC.World.rowWidths = function() {
   averageWidth = Math.max(averageWidth, CCC.World.panelHeight);
   var smallWidth = Math.round(averageWidth * 0.9);
   var largeWidth = averageWidth * 2 - smallWidth;
+  averageWidth -= panelBloat;
+  smallWidth -= panelBloat;
+  largeWidth -= panelBloat;
   // Build an array of lengths.  Add in matching pairs.
   var panels = [];
   for (var i = 0; i < Math.floor(panelCount / 2); i++) {
