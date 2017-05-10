ce0dbc191854e3cd980e86a6d81dd18c592b5ce1
Christopher Allen <cpcallen+git@google.com>
2017-05-10 13:33:28 +0100

    Implement flatType() to produce serialisable versions of arbitrary Go types
    There are a few limitations (e.g. no Functions or Channels) but they
    are not relevant to our use-case.

---
+++ server/serialize/serialize.go
@@ -0,0 +1,104 @@
+/* Copyright 2017 Google Inc.
+ * https://github.com/NeilFraser/CodeCity
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// serialise implements a mechanism to convert arbitrary Go data,
+// possibly including shared substructures, into a slice of objects
+// that can be serialised using encoding.json or the like.
+//
+// BUG(cpcallen): does not handle internal pointers (pointers to array
+// or struct element) correctly.
+//
+// BUG(cpcallen): does not handle shared backing arrays for slices (or
+// strings) correctly.
+package serialize
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// tID is a string uniquely identifying a type
+type tID string
+
+// ref replaces pointers in flattened types
+type ref int
+
+// tagged replaces interface types in flattened types
+type tagged struct {
+	T tID
+	V interface{}
+}
+
+// flatType takes a reflect.Type and returns a substitute reflect.Type
+// that can store the same data but is more suited for serialisation
+// using encoding/json (and similar):
+//
+// - Pointer types replaced with ref (numeric object ID), so circular
+// data and shared substructure can be flattened.
+//
+// - Interfaces are replaced with struct containign explicit type ID
+//
+// - Structs have all fields exported
+//
+// - Maps with non-string key types replaced by slice of 2-member struct
+func flatType(typ reflect.Type) reflect.Type {
+	switch typ.Kind() {
+	case reflect.Invalid:
+		panic("Invalid Kind")
+	case reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
+		reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128, reflect.String:
+		return typ
+	case reflect.Array:
+		return reflect.ArrayOf(typ.Len(), flatType(typ.Elem()))
+	case reflect.Interface:
+		return reflect.TypeOf(tagged{"", nil})
+	case reflect.Map:
+		// If the map key type is a string then we mostly leave it
+		// alone; other maps become a slice of {key, value} pairs.
+		if typ.Key().Kind() == reflect.String {
+			return reflect.MapOf(typ.Key(), flatType(typ.Elem()))
+		} else {
+			var fields = make([]reflect.StructField, 2)
+			fields[0].Name = "K"
+			fields[0].Type = flatType(typ.Key())
+			fields[0].Tag = `json:"k"`
+			fields[1].Name = "V"
+			fields[1].Type = flatType(typ.Elem())
+			fields[1].Tag = `json:"v"`
+			return reflect.SliceOf(reflect.StructOf(fields))
+		}
+	case reflect.Ptr:
+		return reflect.TypeOf((*ref)(nil)).Elem() // *whatever => ref
+	case reflect.Slice:
+		return reflect.SliceOf(flatType(typ.Elem()))
+	case reflect.Struct:
+		var fields []reflect.StructField
+		for i := 0; i < typ.NumField(); i++ {
+			f := typ.Field(i)
+			f.Tag = reflect.StructTag(fmt.Sprintf(`json:"%s"`, f.Name))
+			f.Name = "F_" + f.Name
+			f.PkgPath = ""
+			f.Type = flatType(f.Type)
+			fields = append(fields, f)
+		}
+		return reflect.StructOf(fields)
+	case reflect.Chan, reflect.Func, reflect.UnsafePointer:
+		panic("Not implemented")
+	default:
+		panic("Invalid Kind")
+	}
+}
---
+++ server/serialize/serialize_test.go
@@ -0,0 +1,132 @@
+/* Copyright 2017 Google Inc.
+ * https://github.com/NeilFraser/CodeCity
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package serialize
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFlatTypeSimple(t *testing.T) {
+	// All these should not be changed by flatType():
+	var cases = []interface{}{
+		int(0),
+		int8(0),
+		int16(0),
+		int32(0),
+		int64(0),
+		uint(0),
+		uint8(0),
+		uint16(0),
+		uint32(0),
+		uint64(0),
+		uintptr(0),
+		float32(0),
+		float64(0),
+		complex64(0 + 0i),
+		complex128(0 + 0i),
+		string(""),
+		[3]int{0, 0, 0},
+		[]int{0, 0, 0},
+	}
+	for _, c := range cases {
+		var typ = reflect.TypeOf(c)
+		if r := flatType(typ); r != typ {
+			t.Errorf("flatType(%s) == %s (expected %[1]s)", typ.String(), r.String())
+		}
+	}
+}
+
+func TestFlatTypePtr(t *testing.T) {
+	var cases = []struct {
+		in  interface{}
+		exp interface{}
+	}{
+		{(*int)(nil), ref(0)},
+		{[...]*int{nil, nil, nil}, [...]ref{0, 0, 0}},
+		{[]*int{nil, nil, nil}, []ref{}},
+		{new([3]int), ref(0)},
+		{new([]int), ref(0)},
+	}
+	for _, c := range cases {
+		tin := reflect.TypeOf(c.in)
+		texp := reflect.TypeOf(c.exp)
+		if r := flatType(tin); r != texp {
+			t.Errorf("flatType(%s) == %s (expected %s)", tin, r, texp)
+		}
+	}
+}
+
+func TestFlatTypeMap(t *testing.T) {
+	var msi map[string]*int
+	typ := reflect.TypeOf(msi)
+	if r := flatType(typ); r != reflect.TypeOf(make(map[string]ref)) {
+		t.Errorf("flatType(map[string]*int) == %s (expected map[string]serialize.ref)", r.String())
+	}
+
+	var mis map[int]string
+	typ = reflect.TypeOf(mis)
+	flat := flatType(typ)
+	if flat.Kind() != reflect.Slice ||
+		flat.Elem().Kind() != reflect.Struct ||
+		flat.Elem().Field(0).Type != reflect.TypeOf(0) ||
+		flat.Elem().Field(1).Type != reflect.TypeOf("") {
+		t.Errorf("flatType(map[int]string) == %s (expected some []struct{int, string})", flat.String())
+	}
+}
+
+func TestFlatTypeStruct(t *testing.T) {
+	var s struct {
+		s string
+		i *int
+	}
+	typ := reflect.TypeOf(s)
+	flat := flatType(typ)
+	if flat.Kind() != reflect.Struct ||
+		flat.Field(0).Type != reflect.TypeOf("") ||
+		flat.Field(1).Type != reflect.TypeOf(ref(0)) {
+		t.Errorf("flatType(struct{string, int}) == %s (expected some struct{string, ref})", flat.String())
+	}
+	if !('A' < flat.Field(0).Name[0] && flat.Field(0).Name[0] < 'Z') ||
+		!('A' < flat.Field(1).Name[0] && flat.Field(1).Name[0] < 'Z') {
+		t.Errorf("flatType(struct{string, int}) == %s (expected exported fields)", flat.String())
+	}
+}
+
+func TestFlatTypeInterface(t *testing.T) {
+	// reflect.TypeOf() called on an interface looks at the type of
+	// the thing *in* the interface, not the interface itself, so we
+	// use a little cheat to get an actual interface type to pass to
+	// flatType():
+	var si struct {
+		im interface {
+			method()
+		}
+		i interface{}
+	}
+	typ := reflect.TypeOf(si).Field(0).Type
+	flat := flatType(typ)
+	if flat.Kind() != reflect.Struct ||
+		flat.Field(0).Type != reflect.TypeOf(tID("")) ||
+		flat.Field(1).Type != reflect.TypeOf(si).Field(1).Type {
+		t.Errorf("flatType(interface{...} == %s (expected some struct{tID, interface{})", flat.String())
+	}
+	if !('A' < flat.Field(0).Name[0] && flat.Field(0).Name[0] < 'Z') ||
+		!('A' < flat.Field(1).Name[0] && flat.Field(1).Name[0] < 'Z') {
+		t.Errorf("flatType(interface{...} == %s (expected exported fields)", flat.String())
+	}
+}
