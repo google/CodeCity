google/CodeCity

commit e434075c596a57aa6dd64e2d43d20ee28e9b3a30

Fix split.  Fix 'this' in call and apply.  Fix eval.

server/interpreter.js
@@ -306,7 +306,6 @@ Interpreter.prototype.initFunction = function(scope) {
             'CreateListFromArrayLike called on non-object');
       }
     }
-    state.doneArgs_ = true;
     state.doneExec_ = false;
   };
   this.setNativeFunctionPrototype(this.FUNCTION, 'apply', wrapper);
@@ -323,7 +322,6 @@ Interpreter.prototype.initFunction = function(scope) {
     for (var i = 1; i < arguments.length; i++) {
       state.arguments_.push(arguments[i]);
     }
-    state.doneArgs_ = true;
     state.doneExec_ = false;
   };
   this.setNativeFunctionPrototype(this.FUNCTION, 'call', wrapper);
@@ -904,7 +902,7 @@ Interpreter.prototype.initString = function(scope) {
       separator = separator.data;
     }
     var jsList = this.split(separator, limit);
-    return thisInterpreter.pseudoToNative(jsList);
+    return thisInterpreter.nativeToPseudo(jsList);
   };
   this.setNativeFunctionPrototype(this.STRING, 'split', wrapper);
 
@@ -2309,14 +2307,6 @@ Interpreter.prototype['stepCallExpression'] = function() {
       state.n_++;
       return;
     }
-    state.doneArgs_ = true;
-  }
-  if (!state.doneExec_) {
-    state.doneExec_ = true;
-    var func = state.func_;
-    if (!func || !func.isObject) {
-      this.throwException(this.TYPE_ERROR, func + ' is not a function');
-    }
     // Determine value of 'this' in function.
     if (state.node['type'] === 'NewExpression') {
       if (func.illegalConstructor) {
@@ -2332,6 +2322,14 @@ Interpreter.prototype['stepCallExpression'] = function() {
     } else {
       // Global function, 'this' is undefined.
       state.funcThis_ = undefined;
+    }
+    state.doneArgs_ = true;
+  }
+  if (!state.doneExec_) {
+    state.doneExec_ = true;
+    var func = state.func_;
+    if (!func || !func.isObject) {
+      this.throwException(this.TYPE_ERROR, func + ' is not a function');
     }
     var funcNode = func.node;
     if (funcNode) {
@@ -2377,12 +2375,8 @@ Interpreter.prototype['stepCallExpression'] = function() {
       var code = state.arguments_[0];
       if (!code) {  // eval()
         state.value = undefined;
-      } else if (!code.isPrimitive) {
-        // JS does not parse String objects:
-        // eval(new String('1 + 1')) -> '1 + 1'
-        state.value = code;
       } else {
-        var ast = acorn.parse(code.toString(), Interpreter.PARSE_OPTIONS);
+        var ast = acorn.parse(String(code), Interpreter.PARSE_OPTIONS);
         state = {
           node: {type: 'EvalProgram_', body: ast['body']}
         };

commit d21f7eedf3680c0591cac798a917e8b44ab3d8de

Fix stepCallExpression broken by e434075c596a57aa6dd64e2d43d20ee28e9b3a30
Tests newHackNotAvailable, ObjectDefinePropertiesNoArgs and
ObjectDefinePropertiesNonObject were all crashing because
`func.properties.illegalConstructor` throws a TypeError when func is
not an object (including when it is undefined because it is an
uninitialised variable).

Fix this and add a test for new to catch any similarly blatant
regression.

server/interpreter.js
@@ -2292,7 +2292,8 @@ Interpreter.prototype['stepCallExpression'] = function() {
       state.func_ = this.getValue(func);
       state.components_ = func;
     } else {
-      // Already evaluated function: (function(){...})();
+      // Callee was not an Identifier or MemberExpression, and is
+      // already fully evaluated.
       state.func_ = func;
     }
     state.arguments_ = [];
@@ -2309,7 +2310,10 @@ Interpreter.prototype['stepCallExpression'] = function() {
     }
     // Determine value of 'this' in function.
     if (state.node['type'] === 'NewExpression') {
-      if (func.illegalConstructor) {
+      var func = state.func_;
+      if (!func || !func.isObject) {
+        this.throwException(this.TYPE_ERROR, func + ' is not a function');
+      } else if (func.illegalConstructor) {
         // Illegal: new escape();
         this.throwException(this.TYPE_ERROR, func + ' is not a constructor');
       }
@@ -2328,6 +2332,8 @@ Interpreter.prototype['stepCallExpression'] = function() {
   if (!state.doneExec_) {
     state.doneExec_ = true;
     var func = state.func_;
+    // TODO(fraser): determine if this check is redundant; remove it or add
+    // tests that depend on it.
     if (!func || !func.isObject) {
       this.throwException(this.TYPE_ERROR, func + ' is not a function');
     }

server/tests/testcases.js
@@ -575,6 +575,14 @@ module.exports = [
     `,
     expected: 'TypeError' },
 
+  { name: 'newExpression', src: `
+    function T(x, y) { this.sum += x + y; };
+    T.prototype = { sum: 70 }
+    var t = new T(7, 0.7);
+    t.sum;
+    `,
+    expected: 77.7 },
+
   /******************************************************************/
   // Object and Object.prototype
   