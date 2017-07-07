0f99db2ac22adeb35e588604f3a8e9c339961cf4
Neil Fraser <fraser@google.com>
2017-07-07 13:17:43 +0100

    Fix legalArrayLength to accept stringed hex numbers.

--- server/interpreter.js
+++ server/interpreter.js
@@ -1205,7 +1205,7 @@ Interpreter.prototype.isa = function(child, constructor) {
 Interpreter.legalArrayLength = function(x) {
   var n = x >>> 0;
   // Array length must be between 0 and 2^32-1 (inclusive).
-  return (String(n) === String(x)) ? n : NaN;
+  return (n === Number(x)) ? n : NaN;
 };
 
 /**
