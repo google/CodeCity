ad0597a1f268412163a8177cfee46302fd4fecc0
Christopher Allen <cpcallen+git@google.com>
2017-07-21 11:47:33 +0100

    Implement clearTimeout()

--- server/tests/interpreter_test.js
+++ server/tests/interpreter_test.js
@@ -757,10 +757,11 @@ exports.testAsync = function(t) {
 };
 
 /**
- * Run a test of the suspend() function:
+ * Run a test of the suspend(), setTimeout() and clearTimeout()
+ * functions.
  * @param {!T} t The test runner object.
  */
-exports.testSuspendSetTimeout = function(t) {
+exports.testThreading = function(t) {
   var src = `
       'before';
       suspend();
@@ -795,4 +796,21 @@ exports.testSuspendSetTimeout = function(t) {
       s;
   `;
   runTest(t, 'setTimeout', src, '12345', undefined, wait);
+
+  src = `
+      var s = '';
+      var tid = setTimeout(function() {
+          s += '2';
+          suspend();
+          s += '4';
+      });
+      s += 1;
+      suspend();
+      s += 3;
+      clearTimeout(tid);
+      suspend();
+      s += 5;
+      s;
+  `;
+  runTest(t, 'clearTimeout', src, '1235', undefined, wait);
 };
