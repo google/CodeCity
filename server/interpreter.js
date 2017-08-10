c00fda803111231d6e2c2ab7adb26a87df425c00
Christopher Allen <cpcallen+git@google.com>
2017-08-10 12:36:47 +0100

    Fix typos

--- server/interpreter.js
+++ server/interpreter.js
@@ -1388,7 +1388,7 @@ Interpreter.prototype.initThreads = function(scope) {
 };
 
 /**
- * Initialize the networking subsystem API
+ * Initialize the networking subsystem API.
  * @param {!Interpreter.Scope} scope Global scope.
  */
 Interpreter.prototype.initNetwork = function(scope) {
@@ -2330,7 +2330,7 @@ Interpreter.prototype.installTypes = function() {
       }
     }
     return false;
-  }
+  };
 
   /**
    * Add a .prototype property to this function object, setting
