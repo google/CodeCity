f5e2ed040227be53ae3216d08dfbf162a80ce737
Neil Fraser <fraser@google.com>
2017-08-23 12:21:26 +0100

    ES6 String functions.

--- server/tests/db/test_01_es6.js
+++ server/tests/db/test_01_es6.js
@@ -23,7 +23,7 @@
  */
 
 
-tests.is = function() {
+tests.ObjectIs = function() {
   console.assert(Object.is('foo', 'foo'), 'equal strings');
   console.assert(Object.is(Array, Array), 'equal objects');
 
@@ -39,3 +39,43 @@ tests.is = function() {
   console.assert(Object.is(-0, -0), 'negative zero');
   console.assert(Object.is(NaN, 0/0), 'NaN');
 };
+
+tests.StringBooleanSearchFunctions = function() {
+  var str = 'To be, or not to be, that is the question.';
+
+  console.assert(str.includes('To be'), 'Includes "To be"');
+  console.assert(str.includes('question'), 'Includes "question"');
+  console.assert(!str.includes('nonexistent'), 'Includes "nonexistent"');
+  console.assert(!str.includes('To be', 1), 'Includes "To be" (1)');
+  console.assert(!str.includes('TO BE'), 'Includes "TO BE"');
+
+  console.assert(str.startsWith('To be'), 'StartsWith "To be"');
+  console.assert(!str.startsWith('not to be'), 'StartsWith "not to be"');
+  console.assert(str.startsWith('not to be', 10), 'StartsWith "not to be" (10)');
+
+  console.assert(str.endsWith('question.'), 'EndsWith "question."');
+  console.assert(!str.endsWith('to be'), 'EndsWith "to be"');
+  console.assert(str.endsWith('to be', 19), 'EndsWith "to be" (19)');
+};
+
+tests.StringRepeat = function() {
+  try {
+    'abc'.repeat(-1);
+    console.assert(false, 'Repeat Negative');
+  } catch (e) {
+    console.assert(e.name === 'RangeError',
+                   'Repeat Negative Error');
+  }
+  console.assert('abc'.repeat(0) === '', 'Repeat 0');
+  console.assert('abc'.repeat(1) === 'abc', 'Repeat 1');
+  console.assert('abc'.repeat(2) === 'abcabc', 'Repeat 2');
+  console.assert('abc'.repeat(3.5) === 'abcabcabc', 'Repeat 3.5');
+  try {
+    'abc'.repeat(1 / 0);
+    console.assert(false, 'RegExpPrototypeTestApplyNonRegExpThrows');
+  } catch (e) {
+    console.assert(e.name === 'RangeError',
+                   'RegExpPrototypeTestApplyNonRegExpThrowsError');
+  }
+};
+
