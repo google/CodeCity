google/CodeCity

commit fd9f0cb1867837dc0aaed66fc2e318a201de0530

Use isFinite in toInteger

server/startup/cc.js
@@ -96,7 +96,7 @@ var PermissionError = new 'PermissionError';
     var number = Number(value);
     if (isNaN(number)) {
       return 0;
-    } else if (number === 0 || number === Infinity || number === -Infinity) {
+    } else if (number === 0 || !isFinite(number) {
       return number;
     }
     return Math.trunc(number);

server/interpreter.js
@@ -2130,7 +2130,7 @@ Interpreter.toInteger = function toInteger(value) {
   var number = Number(value);
   if (isNaN(number)) {
     return 0;
-  } else if (number === 0 || number === Infinity || number === -Infinity) {
+  } else if (number === 0 || !isFinite(number)) {
     return number;
   }
   return Math.trunc(number);

server/startup/es6.js
@@ -88,7 +88,7 @@ var WeakMap = new 'WeakMap';
     var number = Number(value);
     if (isNaN(number)) {
       return 0;
-    } else if (number === 0 || number === Infinity || number === -Infinity) {
+    } else if (number === 0 || !isFinite(number)) {
       return number;
     }
     return Math.trunc(number);

commit ace3f82bb6f0a3086c1f241a239068a0b8f7175b

Fix breakage caused by fd9f0cb
Missing ')' in cc.js caused SyntaxError at startup.

server/startup/cc.js
@@ -96,7 +96,7 @@ var PermissionError = new 'PermissionError';
     var number = Number(value);
     if (isNaN(number)) {
       return 0;
-    } else if (number === 0 || !isFinite(number) {
+    } else if (number === 0 || !isFinite(number)) {
       return number;
     }
     return Math.trunc(number);