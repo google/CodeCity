a1a1ebc191069792bbdeae8c0b2f30b481976cb7
Neil Fraser <fraser@google.com>
2018-06-15 17:16:40 -0700

    Upgrade SVG editor to minimum viable product
    Still lots of desirable features to add, but this allows one to draw anything.

--- static/code/svg.js
+++ static/code/svg.js
@@ -51,6 +51,13 @@ svgEditor.init = function() {
     window.initialSource = undefined;
   }
   svgEditor.resize();
+
+  // Update the toolbox whenever a button is clicked.
+  var buttons = document.querySelectorAll('#toolbox button');
+  var update = svgEditor.updateToolbox.bind(svgEditor, true);
+  for (var i = 0, button; (button = buttons[i]); i++) {
+    button.addEventListener('click', update, false);
+  }
 };
 
 /**
@@ -59,7 +66,7 @@ svgEditor.init = function() {
  */
 svgEditor.resize = function() {
   var top = 0;
-  var left = 100;
+  var left = 80;
   var height = window.innerHeight;
   var width = window.innerWidth - left;
   var container = document.getElementById('editorContainer');
@@ -80,6 +87,7 @@ svgEditor.resize = function() {
  */
 svgEditor.setString = function(xmlString) {
   svgEditor.canvas.setSvgString(xmlString);
+  svgEditor.resize();
 };
 
 /**
@@ -93,11 +101,13 @@ svgEditor.getString = function() {
   // Remove the layer title.
   var title = contentSvg.querySelector('g>title');
   title.parentNode.removeChild(title);
-  // Walk the tree removing IDs.
+  // Walk the tree removing unused properties.
   var tw = document.createTreeWalker(contentSvg, NodeFilter.SHOW_ELEMENT);
   do {
     var node = tw.currentNode;
     node.removeAttribute('id');
+    node.removeAttribute('fill');
+    node.removeAttribute('stroke');
   } while (tw.nextNode());
 
   var source = svgEditor.canvas.svgToString(contentSvg, -1);
@@ -107,5 +117,144 @@ svgEditor.getString = function() {
   return source;
 };
 
+/**
+ * Update the toolbox visualization in response to the editor state.
+ * This function polls the editor state four times a second.
+ * @param {boolean=} force If true, force a redraw.
+ */
+svgEditor.updateToolbox = function(force) {
+  // Highlight the current mode button.
+  var mode = svgEditor.canvas.getMode();
+  if (force ||
+      (mode !== 'resize' && mode !== 'rotate' &&
+      mode !== svgEditor.updateToolbox.oldMode_)) {
+    var stylePathEdit = document.getElementById('mode-pathedit').style;
+    var styleSelect = document.getElementById('mode-select').style;
+    var styleNodeActions = document.getElementById('node-actions').style;
+    if (mode === 'pathedit') {
+      styleSelect.display = 'none';
+      stylePathEdit.display = 'block';
+      styleNodeActions.display = 'block';
+      var styleCloseAction = document.getElementById('close-action').style;
+      var styleOpenAction = document.getElementById('open-action').style;
+      if (svgEditor.canvas.pathActions.closed_subpath) {
+        styleCloseAction.display = 'none';
+        styleOpenAction.display = 'block';
+      } else {
+        styleOpenAction.display = 'none';
+        styleCloseAction.display = 'block';
+      }
+    } else {
+      stylePathEdit.display = 'none';
+      styleSelect.display = 'block';
+      styleNodeActions.display = 'none';
+    }
+    var button = document.getElementById('mode-' +
+                                         svgEditor.updateToolbox.oldMode_);
+    if (button) {
+      button.classList.remove('selected');
+    }
+    svgEditor.updateToolbox.oldMode_ = mode;
+    button = document.getElementById('mode-' + mode);
+    if (button) {
+      button.classList.add('selected');
+    }
+  }
+
+  // Show or hide action buttons that apply to one or more selected elements.
+  var selected = svgEditor.canvas.getSelectedElems();
+  if (selected.length !== svgEditor.updateToolbox.oldSelectedCount_) {
+    var actions = document.getElementById('selected-actions');
+    var singleActions = document.getElementById('selected-single-actions');
+    actions.style.display = selected.length > 0 ? 'block' : 'none';
+    singleActions.style.display = selected.length === 1 ? 'block' : 'none';
+    svgEditor.updateToolbox.oldSelectedCount_ = selected.length;
+  }
+  if (selected.length === 1) {
+    var element = selected[0];
+    document.getElementById('convertpath-action').style.display =
+        element.tagName == 'path' ? 'none' : 'block';
+
+    var fillStroke = svgEditor.getFillStroke(element);
+    var fill = fillStroke[0];
+    var stroke = fillStroke[1];
+
+    var fillRect = document.getElementById('fillRect');
+    var fillNoneStyle = document.getElementById('fillNone').style;
+    if (fill === 'fillNone') {
+      fillNoneStyle.display = 'inline';
+      fillRect.setAttribute('class', 'fillWhite');
+    } else {
+      fillNoneStyle.display = 'none';
+      fillRect.setAttribute('class', fill);
+    }
+
+    var strokeRect = document.getElementById('strokeRect');
+    if (stroke === 'strokeNone') {
+      strokeRect.setAttribute('stroke-width', '1');
+      strokeRect.setAttribute('stroke', '#d40000');
+      strokeRect.setAttribute('class', '');
+    } else {
+      strokeRect.setAttribute('stroke-width', '4');
+      strokeRect.setAttribute('stroke', '');
+      strokeRect.setAttribute('class', stroke);
+    }
+  }
+};
+
+svgEditor.updateToolbox.oldMode_ = '';
+svgEditor.updateToolbox.oldSelectedCount_ = 0;
+
+/**
+ * Find the fill and stroke for an element.
+ * @param {!Element} element Element with style.
+ * @return {!Array<string>} Array with normalized fill and stroke strings.
+ */
+svgEditor.getFillStroke = function(element) {
+  if (!element) throw TypeError('Element not provided');
+  var classes = element.getAttribute('class') || '';
+  classes = classes.replace(/Gray/g, 'Grey');
+  var fill = classes.match(/\b(fill(None|White|Black|Grey))\b/);
+  fill = fill ? fill[1] : 'fillNone';
+  var stroke = classes.match(/\b(stroke(None|White|Black|Grey))\b/);
+  stroke = stroke ? stroke[1] : 'strokeBlack';
+  return [fill, stroke];
+};
+
+/**
+ * Rotate the fill in the currently selected object between allowed options.
+ */
+svgEditor.changeFill = function() {
+  var element = svgEditor.canvas.getSelectedElems()[0];
+  var fill = svgEditor.getFillStroke(element)[0];
+  var fillOptions = ['fillNone', 'fillBlack', 'fillGrey', 'fillWhite'];
+  var fillIndex = Math.max(fillOptions.indexOf(fill), 0);
+  fillIndex++;
+  fillIndex %= fillOptions.length;
+  var classes = element.getAttribute('class') || '';
+  classes = classes.replace(/\bfill\w+\b/g, '');
+  classes += ' ' + fillOptions[fillIndex];
+  classes = classes.replace(/\s+/g, ' ').trim();
+  element.setAttribute('class', classes);
+};
+
+/**
+ * Rotate the stroke in the currently selected object between allowed options.
+ */
+svgEditor.changeStroke = function() {
+  var element = svgEditor.canvas.getSelectedElems()[0];
+  var stroke = svgEditor.getFillStroke(element)[1];
+  var strokeOptions = ['strokeBlack', 'strokeGrey', 'strokeWhite', 'strokeNone'];
+  var strokeIndex = Math.max(strokeOptions.indexOf(stroke), 0);
+  strokeIndex++;
+  strokeIndex %= strokeOptions.length;
+  var classes = element.getAttribute('class') || '';
+  classes = classes.replace(/\bstroke\w+\b/g, '');
+  classes += ' ' + strokeOptions[strokeIndex];
+  classes = classes.replace(/\s+/g, ' ').trim();
+  element.setAttribute('class', classes);
+};
+
 window.addEventListener('load', svgEditor.init);
 window.addEventListener('resize', svgEditor.resize);
+setInterval(svgEditor.updateToolbox, 250);
