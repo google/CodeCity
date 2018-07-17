ddbd2f3a1f8f963bdb87ddc6037106cd4c653b61
Christopher Allen <cpcallen+git@gmail.com>
2018-07-17 15:06:00 -0700

    Rewrite dumpBinding; make idempotent
    Rewrite Dumper.p.dumpBinding to make it clearer and so that if it is asked to do work that is alreay done the empty string will be returned.

--- server/tests/dump_test.js
+++ server/tests/dump_test.js
@@ -148,7 +148,7 @@ exports.testDumperPrototypeDumpBinding = function(t) {
       var f2 = function(arg) {};
       f2.f3 = function f4(arg) {};
       var obj = {a: 1, b: 2, c:3};
-      var arr = [42, 69, 105];
+      var arr = [42, 69, 105, obj];
       var date = new Date('1975-07-27');
       var re = /foo/ig;
   `);
@@ -156,26 +156,28 @@ exports.testDumperPrototypeDumpBinding = function(t) {
   const func = intrp.global.get('foo');
 
   const cases = [  // Order matters.
-    ['Object', Do.DECL, 'var Object;'],
-    ['Object', Do.SET, "Object = new 'Object';"],
-    ['f1', Do.DECL, 'var f1;'],
-    // Actually want 'function f1(arg) {};'.
-    ['f1', Do.SET, 'f1 = function f1(arg) {};'],
-    ['f2', Do.SET, 'var f2 = function(arg) {};'],
-    ['f2.f3', Do.DECL, 'f2.f3 = undefined;'],
-    ['f2.f3', Do.SET, 'f2.f3 = function f4(arg) {};'],
-    // Actually want 'var obj = {a: 1, b:2, c: 3};'.
-    ['obj', Do.SET, 'var obj = {};'],
-    // Actually want 'var arr = [42, 69, 105];'
-    ['arr', Do.SET, 'var arr = [];'],
-    ['date', Do.SET, "var date = new Date('1975-07-27T00:00:00.000Z');"],
-    ['re', Do.SET, 'var re = /foo/gi;'],
+    ['Object', Do.DECL, 'var Object;\n'],
+    ['Object', Do.DECL, ''],
+    ['Object', Do.SET, "Object = new 'Object';\n"],
+    ['Object', Do.SET, ''],
+    ['f1', Do.DECL, 'var f1;\n'],
+    // Actually want 'function f1(arg) {};\n'.
+    ['f1', Do.SET, 'f1 = function f1(arg) {};\n'],
+    ['f2', Do.SET, 'var f2 = function(arg) {};\n'],
+    ['f2.f3', Do.DECL, 'f2.f3 = undefined;\n'],
+    ['f2.f3', Do.SET, 'f2.f3 = function f4(arg) {};\n'],
+    // Actually want 'var obj = {a: 1, b:2, c: 3};\n'.
+    ['obj', Do.SET, 'var obj = {};\n'],
+    // Actually want 'var arr = [42, 69, 105, obj];\n'
+    ['arr', Do.SET, 'var arr = [];\n'],
+    ['date', Do.SET, "var date = new Date('1975-07-27T00:00:00.000Z');\n"],
+    ['re', Do.SET, 'var re = /foo/gi;\n'],
   ];
   for (const tc of cases) {
     const parts = toParts(tc[0]);
     var r = dumper.dumpBinding(parts, tc[1]);
-    t.expect(
-        util.format('Dumper.p.dumpBinding(%o, %o)', parts, tc[1]), r, tc[2]);
+    t.expect(util.format('Dumper.p.dumpBinding(%o, %o)', parts, tc[1]),
+        r, tc[2]);
   }
 
 };
