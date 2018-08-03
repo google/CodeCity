c503bb7f78eb5b5b6d12567d8f8386488cae27aa
Christopher Allen <cpcallen+git@gmail.com>
2018-08-03 16:09:34 -0400

    Actually parse selectors
    Selectors are a regular language so we definitely don't need a stack to parse them.  In principle we could write a regexp for them, but in practice it gets rather hairy, so use the traditional approach of a regexp matcher to tokenize and a DFA to parse.

--- server/selector.js
+++ server/selector.js
@@ -50,7 +50,7 @@ var Selector = function(s) {
     }
   } else if (typeof s === 'string') {
     // Parse selector text.
-    parts = s.split('.');
+    parts = parse(s);
   } else {
     throw TypeError('Not a selector or parts array');
   }
@@ -115,6 +115,137 @@ Selector.prototype.toString = function() {
 
 /** @typedef {string} */
 Selector.Part;
+
+
+/**
+ * Parse a selector into an array of Parts.
+ * @return !Array
+ */
+var parse = function(selector) {
+  var tokens = tokenize(selector);
+  var parts = [];
+  /** @enum {number} */
+  var State = {START: 0, GOOD: 1, DOT: 2, BRACKET: 3, SUBSCRIPT: 4};
+  var state = State.START;
+  for (var i = 0; i < tokens.length; i++) {
+    var token = tokens[i];
+    if (token.type === 'whitespace') continue;
+    switch (state) {
+      case State.START:
+        if (token.type !== 'id') {
+          throw new SyntaxError('Selector must start with an identifier');
+        }
+        parts.push(token.raw);
+        state = State.GOOD;
+        break;
+
+      case State.GOOD:
+        if (token.type === '.') {
+          state = State.DOT;
+        } else if (token.type === '[') {
+          state = State.BRACKET;
+        } else {
+          throw new SyntaxError(
+              'Invalid token ' + code.quote(token.raw)  + ' in selector');
+        }
+        break;
+
+      case State.DOT:
+        if (token.type !== 'id') {
+          throw SyntaxError(
+              '"." must be followed by identifier in selector');
+        }
+        parts.push(token.raw);
+        state = State.GOOD;
+        break;
+
+      case State.BRACKET:
+        if (token.type === 'number') {
+          parts.push(String(token.raw));
+        } else if (token.type === 'str') {
+          parts.push(String(token.value));
+        } else {
+          throw new SyntaxError('"[" must be followed by numeric or string ' +
+              'literal in selector');
+        }
+        state = State.SUBSCRIPT;
+        break;
+
+      case State.SUBSCRIPT:
+        if (token.type !== ']') {
+          throw new SyntaxError(
+              'Invalid token ' + code.quote(token.raw)  + ' after subscript');
+        }
+        state = State.GOOD;
+        break;
+
+      default:
+        throw new Error('Invalid State in parse??');
+    }
+  }
+  if (state !== State.GOOD) {
+    throw new SyntaxError('Incomplete selector ' + selector);
+  }
+  return parts;
+};
+
+/** @typedef {{type: string,
+ *             raw: string,
+ *             valid: boolean,
+ *             index: number,
+ *             value: (string|number|undefined)}}
+ */
+var Token;
+
+/**
+ * Tokenizes a selector string.  Throws a SyntaxError if any text is
+ * found which does not form a valid token.
+ * @param {string} selector A selector string.
+ * @return {!Array<Token>} An array of tokens.
+ */
+var tokenize = function(selector) {
+  var REs = {
+    whitespace: /\s+/y,
+    '.': /\./y,
+    id: /[A-Za-z_]\w*/y,  // See also identifierRE below.
+    number: /\d+/y,
+    '[': /\[/y,
+    ']': /\]/y,
+    str: new RegExp(code.regexps.string, 'y'),
+  };
+  
+  var tokens = [];
+  NEXT_TOKEN: for (var index = 0; index < selector.length; ) {
+    for (var tokenType in REs) {
+      if (!REs.hasOwnProperty(tokenType)) continue;
+      var re = REs[tokenType];
+      re.lastIndex = index;
+      var m = re.exec(selector);
+      if (!m) continue;  // No match.  Try next regexp.
+      tokens.push({
+        type: tokenType,
+        raw: m[0],
+        valid: true,
+        index: index,
+      });
+      index = re.lastIndex;
+      continue NEXT_TOKEN;
+    }
+    // No token matched.
+    throw new SyntaxError('invalid selector ' + selector);
+  }
+
+  // Postprocess token list to get values.
+  for(var i = 0; i < tokens.length; i++) {
+    var token = tokens[i];
+    if (token.type === 'number') {
+      token.value = Number(token.raw);
+    } else if (token.type === 'str') {
+      token.value = code.parseString(token.raw);
+    }
+  }
+  return tokens;
+};
 
 /**
  * RegExp matching valid JavaScript identifiers.
--- server/tests/selector_test.js
+++ server/tests/selector_test.js
@@ -38,14 +38,21 @@ exports.testSelector = function(t) {
     [['foo'], 1, 'foo', 'foo', 'foo'],
 
     ['foo.bar', 2, 'foo', 'bar', 'foo.bar'],
+    ['foo["bar"]', 2, 'foo', 'bar', 'foo.bar'],
+    ["foo['bar']", 2, 'foo', 'bar', 'foo.bar'],
     [['foo', 'bar'], 2, 'foo', 'bar', 'foo.bar'],
 
+    ['foo[42]', 2, 'foo', '42', 'foo[42]'],
     [['foo', '42'], 2, 'foo', '42', 'foo[42]'],
 
+    ['foo["bar baz"]', 2, 'foo', 'bar baz', "foo['bar baz']"],
+    ["foo['bar baz']", 2, 'foo', 'bar baz', "foo['bar baz']"],
     [['foo', 'bar baz'], 2, 'foo', 'bar baz', "foo['bar baz']"],
 
+    ["foo['\"\\'\"']", 2, 'foo', '"\'"', "foo['\"\\'\"']"],
     [['foo', '"\'"'], 2, 'foo', '"\'"', "foo['\"\\'\"']"],
 
+    ['foo["\'\\"\'"]', 2, 'foo', "'\"'", 'foo["\'\\"\'"]'],
     [['foo', "'\"'"], 2, 'foo', "'\"'", 'foo["\'\\"\'"]'],
 
     ['foo.bar.baz', 3, 'foo', 'baz', 'foo.bar.baz'],
