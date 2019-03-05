1d14c48e076e961046b1a4395c3cd4f4b9fd7d00
Christopher Allen <cpcallen+git@gmail.com>
2019-03-05 14:58:20 +0000

    Refactor for readability
    - Use fs.{read,write}FileSync methods at startup when usual fs async behaviour is not needed and just makes code convoluted.
    
    - Reorder conditionals to (mostly) reduce indentation.
    
    - Make name of config file a configuration constant.
    
    - Pretty-print with clang-format.

--- login/loginServer.js
+++ login/loginServer.js
@@ -32,6 +32,9 @@ var google = require('googleapis');
 var http = require('http');
 var URL = require('url').URL;
 
+// Configuration constants.
+var configFileName = 'loginServer.cfg';
+
 // Global variables.
 var CFG = null;
 var oauth2Client;
@@ -47,17 +50,18 @@ var loginUrl;
 function serveFile(response, filename, subs) {
   fs.readFile(filename, 'utf8', function(err, data) {
     if (err) {
-      response.statusCode = 500;
       console.log(err);
-      response.end('Unable to load file: ' + filename + '\n' + err);
-    }
-    // Inject substitutions.
-    for (var name in subs) {
-      data = data.replace(name, subs[name]);
+      response.statusCode = 500;
+      data = 'Unable to load file: ' + filename + '\n' + err;
+    } else {
+      // Inject substitutions.
+      for (var name in subs) {
+        data = data.replace(name, subs[name]);
+      }
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/html');
     }
     // Serve page to user.
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/html');
     response.end(data);
   });
 }
@@ -71,121 +75,123 @@ function handleRequest(request, response) {
   if (request.connection.remoteAddress != '127.0.0.1') {
     // This check is redundant, the server is only accessible to
     // localhost connections.
-    console.log('Rejecting connection from ' + request.connection.remoteAddress);
+    console.log(
+        'Rejecting connection from ' + request.connection.remoteAddress);
     response.end('Connection rejected.');
     return;
   }
-
-  if (request.url == CFG.loginPath) {
-    var subs = {'<<<LOGIN_URL>>>': loginUrl};
-    serveFile(response, 'login.html', subs);
-
-  } else if (request.url.startsWith(CFG.loginPath + '?code=')) {
-    var code = new URL(request.url, CFG.redirectUri).searchParams.get('code');
-    oauth2Client.getToken(code, function(err, tokens) {
-      // Now tokens contains an access_token and an optional refresh_token. Save them.
+  // Only serve the page specified by loginPath.
+  if (!request.url.startsWith(CFG.loginPath)) {
+    response.statusCode = 404;
+    response.end(`Unknown Login URL: ${request.url}`);
+    return;
+  }
+  // No query parameters?  Serve login.html.
+  if (request.url === CFG.loginPath) {
+    serveFile(response, 'login.html', {'<<<LOGIN_URL>>>': loginUrl});
+    return;
+  }
+  var code = new URL(request.url, CFG.redirectUri).searchParams.get('code');
+  oauth2Client.getToken(code, function(err, tokens) {
+    if (err) {
+      console.log(err);
+      response.statusCode = 500;
+      response.end('Google Authentication fail: ' + err);
+      return;
+    }
+    // Now tokens contains an access_token and an optional
+    // refresh_token. Save them.
+    oauth2Client.setCredentials(tokens);
+    var oauth2Api = google.oauth2('v2');
+    oauth2Api.userinfo.v2.me.get({auth: oauth2Client}, function(err, data) {
       if (err) {
         console.log(err);
         response.statusCode = 500;
-        response.end('Google Authentication fail: ' + err);
+        response.end('Google Userinfo fail: ' + err);
         return;
       }
-      oauth2Client.setCredentials(tokens);
-
-      var oauth2Api = google.oauth2('v2');
-      oauth2Api.userinfo.v2.me.get({auth: oauth2Client},
-        function(err, data) {
-          if (err) {
-            console.log(err);
-            response.statusCode = 500;
-            response.end('Google Userinfo fail: ' + err);
-            return;
-          }
-          // Convert the Google ID into one unique for Code City.
-          var id = CFG.password + data.id;
-          id = crypto.createHash('sha512').update(id).digest('hex');
-          // Create anti-tampering hash as checksum.
-          var checksum = CFG.password + id;
-          checksum = crypto.createHash('sha').update(checksum).digest('hex');
-          // For future reference, the user's email address is: data.email
-          response.writeHead(302, {  // Temporary redirect
-              'Set-Cookie': 'ID=' + id + '_' + checksum + '; HttpOnly;',
-              'Location': CFG.connectPath
-           });
-          response.end('Login OK.  Redirecting.');
-          console.log('Accepted xxxx' + id.substring(id.length - 4));
-        });
+      // Convert the Google ID into one unique for Code City.
+      var id = CFG.password + data.id;
+      id = crypto.createHash('sha512').update(id).digest('hex');
+      // Create anti-tampering hash as checksum.
+      var checksum = CFG.password + id;
+      checksum = crypto.createHash('sha').update(checksum).digest('hex');
+      // For future reference, the user's email address is: data.email
+      response.writeHead(302, {
+        // Temporary redirect
+        'Set-Cookie': 'ID=' + id + '_' + checksum + '; HttpOnly;',
+        'Location': CFG.connectPath
+      });
+      response.end('Login OK.  Redirecting.');
+      console.log('Accepted xxxx' + id.substring(id.length - 4));
     });
-  } else {
-    response.statusCode = 404;
-    response.end('Unknown Login URL: ' + request.url);
-  }
+  });
 }
 
 /**
- * Read the JSON configuration file.  If none is present, write a stub.
- * When done, call startup.
+ * Read the JSON configuration file and return it.  If none is
+ * present, write a stub and throw an error.
  */
-function configureAndStartup() {
-  var filename = 'loginServer.cfg';
-  fs.readFile(filename, 'utf8', function(err, data) {
-    if (err) {
-      console.log('Configuration file loginServer.cfg not found.  ' +
-                  'Creating new file.  Please edit this file.');
-      data = {
-        // Internal port for this HTTP server.  Nginx hides this from users.
-        httpPort: 7781,
-        // Path to the login page.
-        loginPath: '/login',
-        // Path to the connect page.
-        connectPath: '/connect',
-        // Google's API client ID.
-        clientId: '00000000000-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com',
-        // Google's API client secret.
-        clientSecret: 'yyyyyyyyyyyyyyyyyyyyyyyy',
-        // Full public-facing URL for login page.
-        redirectUri: 'https://example.codecity.world/login',
-        // Domain of connect page.
-        cookieDomain: 'example.codecity.world',
-        // Random password for cookie encryption and salt for login IDs.
-        password: 'zzzzzzzzzzzzzzzz'
-      };
-      data = JSON.stringify(data, null, 2);
-      fs.writeFile(filename, data, 'utf8');
-      return;
-    }
-    data = JSON.parse(data);
-    if (data.password == 'zzzzzzzzzzzzzzzz') {
-      console.log('Configuration file loginServer.cfg not configured.  ' +
-                  'Please edit this file.');
-      return;
-    }
-    CFG = data;
-    startup();
-  });
+function readConfigFile(filename) {
+  let data;
+  try {
+    data = fs.readFileSync(filename, 'utf8');
+  } catch (err) {
+    const template = {
+      // Internal port for this HTTP server.  Nginx hides this from users.
+      httpPort: 7781,
+      // Path to the login page.
+      loginPath: '/login',
+      // Path to the connect page.
+      connectPath: '/connect',
+      // Google's API client ID.
+      clientId: '00000000000-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' +
+          '.apps.googleusercontent.com',
+      // Google's API client secret.
+      clientSecret: 'yyyyyyyyyyyyyyyyyyyyyyyy',
+      // Full public-facing URL for login page.
+      redirectUri: 'https://example.codecity.world/login',
+      // Domain of connect page.
+      cookieDomain: 'example.codecity.world',
+      // Random password for cookie encryption and salt for login IDs.
+      password: 'zzzzzzzzzzzzzzzz'
+    };
+    fs.writeFileSync(filename, JSON.stringify(template, null, 2), 'utf8');
+    throw new Error(
+        `Configuration file ${filename} not found.  ` +
+        'Creating new file.  Please edit this file.');
+  }
+  CFG = JSON.parse(data);
+  if (CFG.password == 'zzzzzzzzzzzzzzzz') {
+    throw new Error(
+        `Configuration file ${filename} not configured.  ` +
+        'Please edit this file.');
+  }
 }
 
 /**
  * Initialize Google's authentication and start up the HTTP server.
  */
 function startup() {
+  try {
+    readConfigFile(configFileName);
+  } catch (err) {
+    console.log(String(err));
+    return;
+  }
+
   // Create an authentication client for our interactions with Google.
-  oauth2Client = new google.auth.OAuth2(
-    CFG.clientId,
-    CFG.clientSecret,
-    CFG.redirectUri
-  );
+  oauth2Client =
+      new google.auth.OAuth2(CFG.clientId, CFG.clientSecret, CFG.redirectUri);
 
   // Precompute Google's login URL.
-  loginUrl = oauth2Client.generateAuthUrl({
-    scope: 'email'
-  });
+  loginUrl = oauth2Client.generateAuthUrl({scope: 'email'});
 
   // Start an HTTP server.
   var server = http.createServer(handleRequest);
-  server.listen(CFG.httpPort, 'localhost', function(){
+  server.listen(CFG.httpPort, 'localhost', () => {
     console.log('Login server listening on port ' + CFG.httpPort);
   });
 }
 
-configureAndStartup();
+startup();
