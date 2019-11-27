83605987dccfca8e3b032fb95cd6ab6af96e160f
Neil Fraser <fraser@google.com>
2019-11-26 21:45:08 -0800

    Revert "Move ping to separate path"
    This reverts commit af3e5c20b7ebf8573db0571a5f5c15db1cf23c6c.

--- static/client/connect.js
+++ static/client/connect.js
@@ -45,7 +45,7 @@ CCC.MAX_HISTORY_SIZE = 1000;
  * Location to send pings to.
  * @constant
  */
-CCC.PING_URL = '/ping';
+CCC.PING_URL = '/connect?ping';
 
 // Properties below this point are not configurable.
 
