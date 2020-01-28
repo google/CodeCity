9bb64c0d3afce66e31d899424c782e2da9d0a93d
Christopher Allen <cpcallen+git@gmail.com>
2020-01-28 19:06:07 +0000

    Fix failing test of connectionListen
    As of macOS 10.14 Mojave, ports <1024 are no longer privileged, so this test was failing because it was successfully able to bind port 22.
    
    There's no obvious alternative candidate for a port that macOS will certainly be listening on, or will certainly refuse to let us bind, so just remove that test case entirely for now.

--- server/tests/interpreter_test.js
+++ server/tests/interpreter_test.js
@@ -1340,10 +1340,9 @@ exports.testNetworking = async function(t) {
   name = 'testConnectionListenThrows';
   src = `
       // Some invalid ports:
-      // * 22 will be in use (or root-only); should be rejected by OS.
       // * 9999 will be in-use by us, should be rejected by connectionListen.
       // * Others are not integers or are out-of-range.
-      var ports = ['foo', {}, -1, 22, 80.8, 9999, 65536];
+      var ports = ['foo', {}, -1, 80.8, 9999, 65536];
       try {
         CC.connectionListen(9999, {});
         for (var i = 0; i < ports.length; i++) {
