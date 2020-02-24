google/CodeCity

commit a963d10c9faccf54978fc00d35fd70928e9ca10e

Use errorNativeToPseudo when relistening fails
When a relisten error occurs, the server prototype object's onError method will be called.  Use errorNativeToPseudo instead of nativeToPseudo to convert the native error.

Added tests to verify this conversion works as intended; it turned out that the whole mechanism was broken, so fix that too.

server/interpreter.js
@@ -413,14 +413,14 @@ Interpreter.prototype.pause = function() {
         var server = this.listeners_[Number(port)];
         server.listen(undefined, function(error) {
           // Something went wrong while re-listening.  Maybe port in use.
-          this.log('net', 'Re-listen on port %s failed: %s: %s', server.port,
-                      error.name, error.message);
+          intrp.log('net', 'Re-listen on port %s failed: %s: %s', server.port,
+                    error.name, error.message);
           // Report this to userland by calling .onError on proto
           // (with this === proto) - for lack of a better option.
           if (!server.owner) return;
           var func = server.proto.get('onError', server.owner);
           if (!(func instanceof intrp.Function)) return;
-          var userError = intrp.nativeToPseudo(error, server.owner);
+          var userError = intrp.errorNativeToPseudo(error, server.owner);
           // TODO(cpcallen:perms): Is server.owner the correct owner
           // for this thread?  Note that this will typically be root,
           // and .onError will therefore get caller perms === root,
@@ -5563,8 +5563,8 @@ Interpreter.prototype.installTypes = function() {
       if (func instanceof intrp.Function && server.owner !== null) {
         // TODO(cpcallen:perms): Is server.owner the correct owner for
         // this thread?  Note that this will typically be root, and
-        // .onError will therefore get caller perms === root, which is
-        // probably dangerous.  Here and several places below.
+        // .onConnect will therefore get caller perms === root, which
+        // is probably dangerous.  Here and several places below.
         intrp.createThreadForFuncCall(
             server.owner, func, obj, [], undefined, server.timeLimit);
       }

server/tests/interpreter_test.js
@@ -1382,6 +1382,7 @@ exports.testNetworking = async function(t) {
       resolve('OK');
    `;
   await runAsyncTest(t, name, src, 'OK', {options: {noLog: ['net']}});
+
   // Run test of the xhr() function using HTTP.
   name = 'testXhrHttp';
   const httpTestServer = http.createServer(function (req, res) {

server/tests/serialize_test.js
@@ -484,8 +484,8 @@ exports.testRoundtripAsync = async function(t) {
   `;
   await runAsyncTest(t, name, src1, src2, '123');
 
-  //  Run a test of the server re-listening to sockets after being
-  //  deserialized.
+  // Run a test of the server re-listening to sockets after being
+  // deserialized.
   name = 'testPostRestoreNetworkInbound';
   src1 = `
       var data = '', conn = {};
@@ -502,7 +502,7 @@ exports.testRoundtripAsync = async function(t) {
   src2 = `
       send();
    `;
-  const onCreate = function(intrp) {
+  const installSend = function(intrp) {
     intrp.global.createMutableBinding('send', intrp.createNativeFunction(
         'send', function() {
           // Send some data to server.
@@ -515,7 +515,33 @@ exports.testRoundtripAsync = async function(t) {
   };
   await runAsyncTest(t, name, src1, src2, 'foobar', {
     options: {noLog: ['net']},
-    onCreate: onCreate,
+    onCreate: installSend,
   });
 
+  // Run a test to verify that the connection object's .error method
+  // is called with a suitable Error object if the previously-listened
+  // port is in use when the interpreter is restarted.
+  name = 'testFailedRelistenFiresError';
+  src1 = `
+      var connection = {onError: function(err) {resolve(err.name);}};
+      CC.connectionListen(8888, connection);
+      resolve();  // Start serialisation roundtrip.
+   `;
+  src2 = `
+      suspend();  // Allow pending onError thread to run.
+      resolve('No error thrown');
+  `;
+  let /** number */ count = 0;
+  let /** ?net.Server */ server = null;
+  const blockPort = function(intrp) {
+    if (count++) {  // Do only when creating post-roundtrip Interpreter.
+      server = new net.Server();
+      server.listen(8888);
+    }
+  };
+  await runAsyncTest(t, name, src1, src2, 'Error', {
+    options: {noLog: ['net']},
+    onCreate: blockPort,
+  });
+  server.close();
 };

commit 59a025738362699a146fb26146384c3c567444fe

Fix issues introduced by PR #319
Regrettably PR #319 introduced two compiler warnings which were not noticed before it was merged.  So:

* Fix call to createListFromArrayLike to include correct perms (used for throwing errors in certain circumstances).

* Fix warning due to functions of different signatures being assigned to wrapper.

server/interpreter.js
@@ -2029,8 +2029,8 @@ Interpreter.prototype.initMath_ = function() {
  */
 Interpreter.prototype.initJSON_ = function() {
   var intrp = this;
-
-  var wrapper = function(text) {
+  var wrapper;
+  wrapper = function(text) {
     try {
       var nativeObj = JSON.parse(text.toString());
     } catch (e) {
@@ -2042,11 +2042,12 @@ Interpreter.prototype.initJSON_ = function() {
 
   wrapper = function(value, replacer, space) {
     var nativeObj = intrp.pseudoToNative(value);
+    var perms = intrp.thread_.perms();
     if (replacer instanceof intrp.Function) {
-      throw new intrp.Error(intrp.thread_.perms(), intrp.TYPE_ERROR,
+      throw new intrp.Error(perms, intrp.TYPE_ERROR,
           'Function replacer on JSON.stringify not supported');
     } else if (replacer instanceof intrp.Array) {
-      replacer = intrp.createListFromArrayLike(replacer);
+      replacer = intrp.createListFromArrayLike(replacer, perms);
       replacer = replacer.filter(function(word) {
         // Spec says we should also support boxed primitives here.
         return typeof word === 'string' || typeof word === 'number';
@@ -2061,7 +2062,7 @@ Interpreter.prototype.initJSON_ = function() {
     try {
       var str = JSON.stringify(nativeObj, replacer, space);
     } catch (e) {
-      throw intrp.errorNativeToPseudo(e, intrp.thread_.perms());
+      throw intrp.errorNativeToPseudo(e, perms);
     }
     return str;
   };